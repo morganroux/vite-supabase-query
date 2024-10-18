0915a00ab4882323973d7b969b97e8626906a7a8
Morgan Roux <roux.morgan@gmail.com>
2024-10-18 20:23:01 +0200

    adding service worker

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,10 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import "@/utils/supabase.ts";
 import queryClient from "./utils/react-query.ts";
 import { Toaster } from "react-hot-toast";
+import { registerServiceWorker } from "./workers/serviceWorker.ts";
 // import './index.css'
+
+registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
